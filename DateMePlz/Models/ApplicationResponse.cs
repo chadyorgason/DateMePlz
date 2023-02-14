3402eebc61f80d4213519178545364610b467c7a
Chad Yorgason <chad.yorgason@gmail.com>
2023-02-13 23:29:49 -0700

    Second commit

--- DateMePlz/Models/ApplicationResponse.cs
+++ DateMePlz/Models/ApplicationResponse.cs
@@ -11,10 +11,15 @@ namespace DateMePlz.Models
         [Key]
         [Required]
         public int MovieID { get; set; }
+        [Required]
         public string Category { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public int Year { get; set; }
+        [Required]
         public string Director { get; set; }
+        [Required]
         public string Rating { get; set; }
         public bool Edited { get; set; }
         public string Lent_To { get; set; }
